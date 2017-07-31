6c4983e172d0f845bc24ce4ff2444bd32626111d
fslic <fslic@DESKTOP-MCATCHJ>
2017-07-31 22:42:15 +0800

    new demos

---
+++ Development_Framework/src/main/java/evolution/controller/dto/Student.java
@@ -0,0 +1,22 @@
+package evolution.controller.dto;
+
+public class Student {
+	private String name;
+	private String gender;
+	public Student(String name, String gender) {
+		this.name = name;
+		this.gender = gender;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+}
--- Development_Framework/src/main/java/evolution/controller/thymeleaf/ThymeleafController.java
+++ Development_Framework/src/main/java/evolution/controller/thymeleaf/ThymeleafController.java
@@ -1,4 +1,8 @@
-package evolution.controller;
+package evolution.controller.thymeleaf;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -6,14 +10,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import evolution.controller.dto.Student;
+
 @Controller
-public class AnyController {
+public class ThymeleafController {
 	@GetMapping("/thymeleaf/get")
 	public String thymeleafGet(Model model, HttpServletRequest request) {
 		String name = request.getParameter("name");
 		String gender = request.getParameter("gender");
+		List<Student> students = new LinkedList<>();
+		students.addAll(Arrays.asList(new Student("Chen", "M"), new Student("Ling", "F")));
 		model.addAttribute("name", name == null ? "Chen" : name);
 		model.addAttribute("gender", gender == null ? "M" : gender);
+		model.addAttribute("age", "27");
+		model.addAttribute("students", students);
 		return "thymeleaf";// Redirects to thymeleaf.html under templates folder.
 	}
 }

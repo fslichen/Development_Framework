2886e9864a6d3642edb32ae5c0bb1dc6dfa0b47a
fslic <fslic@DESKTOP-MCATCHJ>
2017-08-01 23:00:57 +0800

    new demos

--- Development_Framework/src/test/java/spring/boot/AnyClass.java
+++ Development_Framework/src/test/java/spring/boot/AnyClass.java
@@ -4,11 +4,9 @@ import static org.mockito.BDDMockito.given;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
 import evolution.service.injection.AnyService;
 

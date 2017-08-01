7d8f3138ac3e6c2c2442fdb52046962be520c324
chenli <chenli@chens-air>
2017-08-01 17:44:13 +0800

    new demos

---
+++ Development_Framework/src/main/java/evolution/util/MD5HashingExample.java
@@ -0,0 +1,26 @@
+package evolution.util;
+
+import java.security.MessageDigest;
+
+public class MD5HashingExample {
+	public static void main(String[] args) throws Exception {
+		String password = "123456";
+		MessageDigest md = MessageDigest.getInstance("MD5");
+		md.update(password.getBytes());
+		byte byteData[] = md.digest();
+		// Convert the byte to hex format using method 1
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < byteData.length; i++) {
+			sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+		}
+		System.out.println("Digest(in hex format):: " + sb.toString());
+		// Convert the byte to hex format using method 2
+		StringBuffer hexString = new StringBuffer();
+		for (int i = 0; i < byteData.length; i++) {
+			String hex = Integer.toHexString(0xff & byteData[i]);
+			if (hex.length() == 1) hexString.append('0');
+			hexString.append(hex);
+		}
+		System.out.println("Digest(in hex format):: " + hexString.toString());
+	}
+}

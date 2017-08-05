a2ff329fa5354d52e8d77761d112c93b01776830
fslic <fslic@DESKTOP-MCATCHJ>
2017-08-05 18:36:58 +0800

    new demos

--- Development_Framework/src/main/java/evolution/logger/Slf4jDemo.java
+++ Development_Framework/src/main/java/evolution/logger/Slf4jDemo.java
@@ -6,12 +6,13 @@ import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Slf4jLoggerDemo {
-	Logger logger = LoggerFactory.getLogger(Slf4jLoggerDemo.class);
+public class Slf4jDemo {
+	// Logback is used by default in spring boot.
+	Logger logger = LoggerFactory.getLogger(Slf4jDemo.class);
 	
 	// Priority Hierarchy = Trace < Debug < Info < Warn < Error < Fatal
 	public static void main(String[] args) {
-		Slf4jLoggerDemo slf4jLoggerDemo = new Slf4jLoggerDemo();
+		Slf4jDemo slf4jLoggerDemo = new Slf4jDemo();
 		slf4jLoggerDemo.logger.info("Hello World");
 		// {} stands for an argument.
 		slf4jLoggerDemo.logger.info("The time is now {}", new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(new Date()));
---
+++ Development_Framework/src/main/java/evolution/logger/log4j2/Log4jFileDemo.java
@@ -0,0 +1,16 @@
+package evolution.logger.log4j2;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Log4jFileDemo {
+	private static Logger logger = LogManager.getLogger();
+	
+	// Please put log4j2.xml under the class path. (src/main/resources)
+	public static void main(String[] args) {
+		logger.info("The current time is {}.", new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(new Date()));
+	}
+}
